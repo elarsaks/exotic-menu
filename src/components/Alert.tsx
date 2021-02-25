953d713f53af3127be5fefe64d31b8cfb3a0700b
elarsaks <elarsaks@gmail.com>
2021-02-25 22:46:16 +0200

    Create custom alert.

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,33 +1,32 @@
 import React, { FC } from 'react'
+import styled from '@emotion/styled'
 
-export type Kind = 'info' | 'positive' | 'negative' | 'warning'
+export type Kind = 'error' | 'info' | 'success' | 'warning'
 export type KindMap = Record<Kind, string>
 
 const kinds: KindMap = {
-  info: '#5352ED',
-  positive: '#2ED573',
-  negative: '#FF4757',
-  warning: '#FFA502',
+  error: 'rgb(172, 8, 49)',
+  info: 'rgb(8, 19, 177)',
+  success: 'rgb(4, 126, 65)',
+  warning: 'rgb(211, 190, 0)',
 }
 
 export interface AlertProps {
-  /**
-   * Set this to change alert kind
-   * @default info
-   */
-  kind: 'info' | 'positive' | 'negative' | 'warning'
+  kind: 'error' | 'info' | 'success' | 'warning'
 }
 
+const getKind = ({ kind = 'info' }: AlertProps) => kinds[kind]
+const AlertStyled = styled('div')<AlertProps>`
+  background: ${getKind};
+  padding: 20px;
+  margin: 10px;
+  color: white;
+  border-radius: 3px;
+  //border: 2px solid ${getKind};
+`
+
 export const Alert: FC<AlertProps> = ({ children, kind, ...rest }) => (
-  <div
-    style={{
-      padding: 20,
-      background: kinds[kind] || 'white',
-      borderRadius: 3,
-      color: 'white',
-    }}
-    {...rest}
-  >
+  <AlertStyled kind={kind} {...rest}>
     {children}
-  </div>
+  </AlertStyled>
 )
