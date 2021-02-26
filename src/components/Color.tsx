5ee9d87749bf9c0256c3a5c59a3d4268fb72b1f1
elarsaks <elarsaks@gmail.com>
2021-02-27 00:01:25 +0200

    Create Color.mdx.

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,21 +1,22 @@
 import React, { FC } from 'react'
 import styled from '@emotion/styled'
 
-export const error = 'rgb(172, 8, 49)'
+export const error = 'rgb(221, 17, 37)'
 export const info = 'rgb(5, 77, 218)'
 export const success = 'rgb(4, 126, 65)'
-export const warning = 'rgb(211, 162, 0)'
+export const warning = 'rgb(236, 232, 1)'
 
 export interface ColorExampleProps {
-  color: string
+  color: 'error' | 'info' | 'success' | 'warning'
 }
 
 const Example = styled('div')<ColorExampleProps>`
+  display: inline-flex;
+  background: ${(p) => p.color};
   height: 200px;
-  width: 100px;
-  padding: 20px;
+  width: 200px;
   margin: 10px;
-  border-radius: 5px;
+  border-radius: 15px;
 `
 
 export const ColorExample: FC<ColorExampleProps> = ({ color }) => (
