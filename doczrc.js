13f199e2022a16f6c3191a684b6f9cbe902e497a
Elar Saks <elarsaks@gmail.com>
2023-08-15 10:07:23 +0300

    Try again with docz.

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,9 @@
 export default {
-  base: "https://elarsaks.github.io/exotic-menu/",
+  //base: "https://elarsaks.github.io/exotic-menu/",
   typescript: true,
   files: ['**/*.mdx'],
   ignore: ['README.md'],
+  gatsby: {
+    pathPrefix: "/exotic-menu",
+  },
 }
