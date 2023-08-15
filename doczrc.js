f25b51ed2694337c862a8a7fd8ffb8095c73622e
Elar Saks <elarsaks@gmail.com>
2023-08-15 11:22:36 +0300

    Fix doczrc.

--- doczrc.js
+++ doczrc.js
@@ -1,16 +1,13 @@
 import { deep } from '@theme-ui/presets';
 
 export default {
-  base: "exotic-menu/",
+  base: "/exotic-menu/",
   typescript: true,
   files: ['**/*.mdx'],
   ignore: ['README.md'],
-  gatsby: {
-    pathPrefix: "/exotic-menu",
-  },
   themeConfig: {
     ...deep,
-    initialColorModeName: 'dark', 
-    useColorSchemeMediaQuery: false, 
+    initialColorModeName: 'dark',
+    useColorSchemeMediaQuery: false,
   },
-}
+};
